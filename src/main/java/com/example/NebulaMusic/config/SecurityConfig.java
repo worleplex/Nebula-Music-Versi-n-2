691d70031dbbfa060e018b4a92235295e16ac92b
worleplex <guille22izamen@gmail.com>
2026-09-22 18:56:09 -0700

    trabajo completo

--- src/main/java/com/example/NebulaMusic/config/SecurityConfig.java
+++ src/main/java/com/example/NebulaMusic/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
                     .authorizeHttpRequests(auth -> auth
                             .requestMatchers("/", "/index", "/iniciar-sesion", "/registro").permitAll()
                             .requestMatchers("/crear-cuenta", "/autentication").permitAll()
-                            .requestMatchers("/static/**", "/css/**", "/js/**", "/images/**").permitAll()
+                            .requestMatchers("/static/**", "/css/**", "/js/**", "/images/**", "/gridlayout.html").permitAll()
                             .requestMatchers("/", "/index", "/index.html", "/iniciar-sesion",
                                     "/iniciar-sesion.html", "/registro", "/registro.html").permitAll()
                             .anyRequest().authenticated()
@@ -26,7 +26,3 @@ public class SecurityConfig {
         }
 
     }
-
-
-
-
